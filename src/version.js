e3ac6aebc42a22f476b52cc51fdeddea5675f371
theinsideshine <educacion.ta@gmail.com>
2022-04-18 16:01:33 -0300

    v1.0.06 Se agrego cfg de ip por archivo

--- src/version.js
+++ src/version.js
@@ -2,4 +2,5 @@
 // v1.0.05 Se agrego pag about
 //         Se cambio result_x por resultx
 //         Se agrego example 3 y 4
-export  const ver='remote-lab-lib 1.0.05';
+// v1.0.06 Se agrego cfg de ip por archivo
+export  const ver='remote-lab-lib 1.0.06';
